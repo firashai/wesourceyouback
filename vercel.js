6f49eea1da9be70f994a5e0de5a300ee2cc55ea6
firas haidar <haidar_firas013@hotmail.com>
2025-08-18 15:24:33 +0300

    vercel

--- vercel.js
+++ vercel.js
@@ -1,48 +1,67 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { HttpExceptionFilter } from './common/filters/http-exception.filter';
-import { TransformInterceptor } from './common/interceptors/transform.interceptor';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-const { createServer } = require('http');
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+// import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+// import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// const { createServer } = require('http');
+// const serverless = require('serverless-http');
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   // Global pipes
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       whitelist: true,
+//       forbidNonWhitelisted: true,
+//       transform: true,
+//     }),
+//   );
+
+//   // Global filters
+//   app.useGlobalFilters(new HttpExceptionFilter());
+
+//   // Global interceptors
+//   app.useGlobalInterceptors(new TransformInterceptor());
+
+//   // Swagger setup
+//   const config = new DocumentBuilder()
+//     .setTitle('Media Brokerage API')
+//     .setDescription('API for media professionals and companies')
+//     .setVersion('1.0')
+//     .addBearerAuth()
+//     .build();
+//   const document = SwaggerModule.createDocument(app, config);
+//   SwaggerModule.setup('api', app, document);
+//   return serverless(app.getHttpAdapter().getInstance());
+
+// //   await app.listen(process.env.PORT || 3000);
+// }
+// // bootstrap();
+// let server;
+// module.exports = async (req, res) => {
+//   if (!server) {
+//     server = await bootstrap();
+//   }
+//   return server(req, res);
+// };
+
+const { NestFactory } = require('@nestjs/core');
 const serverless = require('serverless-http');
+const { AppModule } = require('./dist/app.module'); // Make sure this path matches your build output
 
-async function bootstrap() {
+let cachedServer;
+
+async function bootstrapServer() {
   const app = await NestFactory.create(AppModule);
-
-  // Global pipes
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
-
-  // Global filters
-  app.useGlobalFilters(new HttpExceptionFilter());
-
-  // Global interceptors
-  app.useGlobalInterceptors(new TransformInterceptor());
-
-  // Swagger setup
-  const config = new DocumentBuilder()
-    .setTitle('Media Brokerage API')
-    .setDescription('API for media professionals and companies')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  await app.init();
   return serverless(app.getHttpAdapter().getInstance());
-
-//   await app.listen(process.env.PORT || 3000);
 }
-// bootstrap();
-let server;
-module.exports = async (req, res) => {
-  if (!server) {
-    server = await bootstrap();
+
+module.exports.handler = async (event, context) => {
+  if (!cachedServer) {
+    cachedServer = await bootstrapServer();
   }
-  return server(req, res);
+  return cachedServer(event, context);
 };
